57fb7f257e611d8d35ca5cc38874c51e9ffc77ea
Matt <msb5pe@virginia.edu>
2020-02-19 18:31:02 -0500

    Model fields

--- login/models.py
+++ login/models.py
@@ -7,3 +7,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
+    token = models.CharField(max_length=200, primary_key=True)
+    last_signin = models.DateTimeField('Last sign-in')
+
+
