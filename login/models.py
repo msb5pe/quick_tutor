6324f53a9e1e9ecdc774518811a896326ac4835d
Arvind Anand <arvindanand1123@gmail.com>
2020-02-19 18:39:20 -0500

    Auto stash before rebase of "dev"

--- login/models.py
+++ login/models.py
@@ -7,5 +7,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
+<<<<<<< Updated upstream
     token = models.CharField(max_length=200, primary_key=True)
     last_signin = models.DateTimeField('Last sign-in')
+=======
+    
+>>>>>>> Stashed changes
