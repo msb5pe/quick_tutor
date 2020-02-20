2f680cce9970095f59d8e8a086022da5ae921b0f
Arvind Anand <arvindanand1123@gmail.com>
2020-02-20 13:34:53 -0500

    Configured admin for login
    Admin should work for login
    The user model now has defaults

--- login/models.py
+++ login/models.py
@@ -2,10 +2,11 @@
 
 
 from django.db import models
+from django.utils import timezone
 
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
-    token = models.CharField(max_length=200, primary_key=True)
-    last_signin = models.DateTimeField('Last sign-in')
+    token = models.CharField(max_length=200, primary_key=True, default='0000000')
+    last_signin = models.DateTimeField('Last sign-in', default=timezone.now)
