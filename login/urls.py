28779a8be9783911a89d4aae8f3398ada478b07b
Arvind Anand <arvindanand1123@gmail.com>
2020-02-24 15:16:30 -0500

    Revert "Merge pull request #13 from uva-cs3240-s20/arvindbranch"
    This reverts commit e928d1c3d496a96f03105dd072c8dd33400443da.

--- login/urls.py
+++ login/urls.py
@@ -9,10 +9,13 @@ from django.conf import settings
 
 from . import views
 from django.conf.urls import include
+from django.contrib.auth import logout
 
 
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     path('', views.index, name='index'),
-    path('logout/', views.logout_success, name='logout')
+    path('logout/', logout, {'': settings.LOGOUT_REDIRECT_URL},
+    name='logout'),
+    path('logout-success/', views.logout_success, name='logout-success')
 ]
