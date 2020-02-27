b5a4acec2b3fa4da30449139c1790704037f3aaf
Matt <msb5pe@virginia.edu>
2020-02-27 17:27:44 -0500

    Basic skeleton of new user

--- login/urls.py
+++ login/urls.py
@@ -14,5 +14,7 @@ from django.conf.urls import include
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     path('', views.index, name='index'),
-    path('logout/', views.logout_success, name='logout')
+    path('logout/', views.logout_success, name='logout'),
+    path('create_user/', views.create_user, name='create_user'),
+    path('profile/', views.profile, name='profile')
 ]
