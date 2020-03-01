bc2642fb7f5556d7e7edca40af53fc1be6163d86
Arvind Anand <arvindanand1123@gmail.com>
2020-03-01 17:20:16 -0500

    Auto stash before rebase of "dev"

--- login/views.py
+++ login/views.py
@@ -6,18 +6,19 @@ from django.http import Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
 
 
 def login_success(request):
     return HttpResponse("You have successfully logged in")
 
 def logout_success(request):
-    return HttpResponse("You have successfully logged out")
+    logout(request)
+    return redirect('index')
 
 def index(request):
     return render(request, 'login/index.html')
-
 
 def create_user(request):
     return render(request, 'login/create_user.html')
