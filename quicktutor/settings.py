21ec141c77387545283784dc7daeb29f561bf793
Arvind Anand <arvindanand1123@gmail.com>
2020-03-01 18:16:24 -0500

    try 7

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -22,10 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
