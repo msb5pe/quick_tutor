14ef8a0685ae7c41e736ba08f11aeefb0fdabcce
Matt <msb5pe@virginia.edu>
2020-03-01 19:08:53 -0500

    Example unit test

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import authenticate
 from django.test import TestCase
+from django.contrib.auth.models import User
+from random import sample
+from django.urls import reverse
 
 # Create your tests here.
+class ExampleTestCase(TestCase):
+    assert(True)
