bd897ab0f22e08f3cd50951f602ceb67c6668a4e
Matt <msb5pe@virginia.edu>
2020-03-02 14:58:15 -0500

    Revert "Auto stash before rebase of "master""
    This reverts commit c795b29a7fece5810b42e5e214c9284b4a74c4d2.

--- login/models.py
+++ login/models.py
@@ -2,12 +2,12 @@
 V1: Login with Google works 
 """
 
+
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    last_login = models.DateTimeField('date created', default=timezone.now)
-    # classes = models.ListCharField(models.CharField)
+
+# class Profiles(models.Model):
+#     name = models.OneToOneField(User, on_delete=models.CASCADE)
+#     email = models.OneToOneField(User,)
