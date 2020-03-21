b713bd088444d888374010354de4c49c8cd88048
Arvind Anand <arvindanand1123@gmail.com>
2020-03-21 19:34:32 -0400

    User Model Changes for Pipeline Integration

--- login/models.py
+++ login/models.py
@@ -1,13 +1,19 @@
 """ 
 V1: Login with Google works 
+V1.2: Adjusted Profile model to include a boolean field for determining if someone has been helped
+    and one for if he/she is a tutor
 """
 
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
 
-# class Profiles(models.Model):
-#     name = models.OneToOneField(User, on_delete=models.CASCADE)
-#     email = models.OneToOneField(User,)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)    
+    classes = ArrayField(models.CharField(max_length=256), size=8)
+    helped = models.BooleanField()
+    tutor = models.BooleanField()
