a97b581f7af712d7bfb52a9471bf8d733b6c55cf
Matt <msb5pe@virginia.edu>
2020-03-22 16:46:58 -0400

    Auto stash before rebase of "dev"

--- login/models.py
+++ login/models.py
@@ -4,10 +4,12 @@ V1: Login with Google works
 
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
-
-
-# class Profiles(models.Model):
-#     name = models.OneToOneField(User, on_delete=models.CASCADE)
-#     email = models.OneToOneField(User,)
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=255, blank=True)
+    date_created = models.DateTimeField('date account created', default=None)
