b29dcec8bb0032fb3143ca21cd9d7931bb103af7
Matt <msb5pe@virginia.edu>
2020-04-05 15:18:04 -0400

    User_profile selects location

--- login/views.py
+++ login/views.py
@@ -8,8 +8,8 @@ from django.views import generic
 from django.utils import timezone
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import logout
-from .models import UserProfile
-from .forms import UserEditForm, ProfileEditForm
+from .models import UserProfile, Location
+from .forms import set_location_form
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm
 
@@ -32,7 +32,29 @@ def class_select_isTutor(request):
     user_profile = UserProfile.objects.filter(user=request.user)[0]
     user_profile.is_tutor = True
     user_profile.save()
-    return render(request, 'login/classes.html')
+    # --- Probably should go somewhere else, this is just to write the code out ------
+    locations_list = Location.objects.order_by('placeName')
+    template = loader.get_template('login/locations.html')
+    context = {'locations_list': locations_list,}
+    return render(request, 'login/locations.html', context)
+    # -------------------------------------------------------------------------
+    # return render(request, 'login/classes.html')
+
+
+def select_location(request):
+    user_profile = UserProfile.objects.filter(user=request.user)[0]
+    try:
+        loc = request.POST.get('location')
+        user_profile.location = loc
+        user_profile.save()
+    except Exception as e:
+        print(e)
+        return render(request, 'login/locations.html', {
+            'error_message': "Empty text fields",
+        })
+    else:
+        return HttpResponse(loc)
+
 
 def class_select_isTutee(request):
     return render(request, 'login/classes.html')
