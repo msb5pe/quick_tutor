de23b9a6f02a5b2e13bd390b5be95f2c92e69e29
Matt <msb5pe@virginia.edu>
2020-04-08 15:11:35 -0400

    Select location

--- login/views.py
+++ login/views.py
@@ -28,10 +28,14 @@ def class_select_isTutor(request):
     return render(request,'login/dept.html')
 
 def class_select_isTutee(request):
+    user_profile = find_user(request.user)
+    user_profile.is_tutor = False
+    user_profile.save()
     return render(request, 'login/dept.html')
 
 def class_selector(request):
     user_profile = find_user(request.user)
+    locations_list = Location.objects.order_by('placeName')
     r = requests.get("http://stardock.cs.virginia.edu/louslist/courses/view/" + request.POST['department'] + "?JSON")
     ret_ary = []
     for line in r.text.split("\n"):
@@ -42,40 +46,20 @@ def class_selector(request):
             pass
     ret_ary = list(set(ret_ary))
     if r.status_code == 200:
-        payload = {'classes': ret_ary}
+        payload = {'classes': ret_ary, 'locations_list': locations_list, }
         return render(request,'login/classes.html', payload)
     return render(request, 'login/dept.html')
 
-def select_location(request):
-    locations_list = Location.objects.order_by('placeName')
-    template = loader.get_template('login/locations.html')
-    context = {'locations_list': locations_list, }
-    return render(request, 'login/locations.html', context)
-
-# Test function to show the selected location
-# Used with location_test.html
-def select_location2(request):
-    user_profile = find_user(request.user)
-    try:
-        loc = request.POST.get('location')
-        user_profile.location = loc
-        user_profile.save()
-    except Exception as e:
-        print(e)
-        return render(request, 'login/locations.html', {
-            'error_message': "Empty text fields",
-        })
-    else:
-        return HttpResponse(loc)
-    # Set location and then redirect!!!
 
 def home_redirect(request):
     user_profile = find_user(request.user)
     classes = request.POST.getlist('classes')
+    location = request.POST.get('location')
     cls_str = classes[0]
     for c in classes[1:]:
         cls_str = cls_str + "," + c
     user_profile.classes = cls_str
+    user_profile.location = location
     user_profile.save()
     return redirect('home:index')
 
